0b5c270488518808f5f94b51914987c1ff9bd867
altruisticnode <altruisticnode@gmail.com>
2023-04-15 19:53:25 -0700

    Wire up core functionalities to the main process

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+import getAccessToken from "./modules/spotify/access-token";
+import getRecentTracks from "./modules/spotify/recent-tracks";
+import writer from "./modules/github/gist-writer";
+
+(async () => {
+    const token = await getAccessToken();
+    const tracks = await getRecentTracks(token);
+    writer(tracks);
+})();
